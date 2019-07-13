dd41df9e20d4d067cd7a04ac89a3d70be95609da
Jon Cram <webignition@gmail.com>
2019-07-13 15:17:28 +0100

    Create PageFactory (#16)

---
+++ src/PageFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace webignition\BasilModelFactory;
+
+use Nyholm\Psr7\Uri;
+use webignition\BasilModel\Identifier\IdentifierInterface;
+use webignition\BasilModel\Page\Page;
+use webignition\BasilModel\Page\PageInterface;
+use webignition\BasilDataStructure\Page as PageData;
+
+class PageFactory
+{
+    /**
+     * @var IdentifierFactory
+     */
+    private $identifierFactory;
+
+    public function __construct(IdentifierFactory $identifierFactory)
+    {
+        $this->identifierFactory = $identifierFactory;
+    }
+
+    public static function create(?IdentifierFactory $identifierFactory = null): PageFactory
+    {
+        return new PageFactory($identifierFactory ?? IdentifierFactory::createFactory());
+    }
+
+    /**
+     * @param PageData $pageData
+     *
+     * @return PageInterface
+     *
+     * @throws MalformedPageElementReferenceException
+     */
+    public function createFromPageData(PageData $pageData): PageInterface
+    {
+        $uriString = $pageData->getUrl();
+        $elementData = $pageData->getElements();
+
+        $uri = new Uri($uriString);
+
+        $elementIdentifiers = [];
+
+        foreach ($elementData as $elementName => $identifierString) {
+            $identifier = $this->identifierFactory->createWithElementReference(
+                $identifierString,
+                $elementName,
+                $elementIdentifiers
+            );
+
+            if ($identifier instanceof IdentifierInterface) {
+                $elementIdentifiers[$elementName] = $identifier;
+            }
+        }
+
+        return new Page($uri, $elementIdentifiers);
+    }
+}
