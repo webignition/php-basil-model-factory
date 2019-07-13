daa67014a83c029e9c733d9b89935273a49e297e
Jon Cram <webignition@gmail.com>
2019-07-13 16:29:48 +0100

    Create StepFactory (#19)

---
+++ src/StepFactory.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace webignition\BasilModelFactory;
+
+use webignition\BasilContextAwareException\ExceptionContext\ExceptionContextInterface;
+use webignition\BasilModel\DataSet\DataSet;
+use webignition\BasilModel\Identifier\IdentifierInterface;
+use webignition\BasilModel\Step\PendingImportResolutionStep;
+use webignition\BasilModel\Step\Step;
+use webignition\BasilModel\Step\StepInterface;
+use webignition\BasilDataStructure\Step as StepData;
+use webignition\BasilModelFactory\Action\ActionFactory;
+
+class StepFactory
+{
+    /**
+     * @var ActionFactory
+     */
+    private $actionFactory;
+
+    /**
+     * @var AssertionFactory
+     */
+    private $assertionFactory;
+
+    /**
+     * @var IdentifierFactory
+     */
+    private $identifierFactory;
+
+    public function __construct(
+        ActionFactory $actionFactory,
+        AssertionFactory $assertionFactory,
+        IdentifierFactory $identifierFactory
+    ) {
+        $this->actionFactory = $actionFactory;
+        $this->assertionFactory = $assertionFactory;
+        $this->identifierFactory = $identifierFactory;
+    }
+
+    public static function createFactory(
+        ?ActionFactory $actionFactory = null,
+        ?AssertionFactory $assertionFactory = null,
+        ?IdentifierFactory $identifierFactory = null
+    ): StepFactory {
+        return new StepFactory(
+            $actionFactory ?? ActionFactory::createFactory(),
+            $assertionFactory ?? AssertionFactory::createFactory(),
+            $identifierFactory ?? IdentifierFactory::createFactory()
+        );
+    }
+
+    /**
+     * @param StepData $stepData
+     *
+     * @return StepInterface
+     *
+     * @throws MalformedPageElementReferenceException
+     */
+    public function createFromStepData(StepData $stepData): StepInterface
+    {
+        $actionStrings = $stepData->getActions();
+        $assertionStrings = $stepData->getAssertions();
+
+        $actions = [];
+        $assertions = [];
+
+        $actionString = '';
+        $assertionString = '';
+
+        try {
+            foreach ($actionStrings as $actionString) {
+                if ('string' === gettype($actionString)) {
+                    $actionString = trim($actionString);
+
+                    if ('' !== $actionString) {
+                        $actions[] = $this->actionFactory->createFromActionString($actionString);
+                    }
+                }
+            }
+
+            foreach ($assertionStrings as $assertionString) {
+                if ('string' === gettype($assertionString)) {
+                    $assertionString = trim($assertionString);
+
+                    if ('' !== $assertionString) {
+                        $assertions[] = $this->assertionFactory->createFromAssertionString($assertionString);
+                    }
+                }
+            }
+        } catch (MalformedPageElementReferenceException $contextAwareException) {
+            $contextAwareException->applyExceptionContext([
+                ExceptionContextInterface::KEY_CONTENT => $assertionString !== '' ? $assertionString : $actionString,
+            ]);
+
+            throw $contextAwareException;
+        }
+
+        $step = new Step($actions, $assertions);
+
+        if ($stepData->getImportName() || $stepData->getDataImportName()) {
+            $step = new PendingImportResolutionStep(
+                $step,
+                $stepData->getImportName(),
+                $stepData->getDataImportName()
+            );
+        }
+
+        $dataArray = $stepData->getDataArray();
+        if (!empty($dataArray)) {
+            foreach ($dataArray as $key => $dataSetData) {
+                $data[$key] = new DataSet($dataSetData);
+            }
+        }
+
+        if (!empty($data)) {
+            $step = $step->withDataSets($data);
+        }
+
+        $elementIdentifiers = [];
+        foreach ($stepData->getElements() as $elementName => $elementIdentifierString) {
+            $elementIdentifier = $this->identifierFactory->create($elementIdentifierString, $elementName);
+
+            if ($elementIdentifier instanceof IdentifierInterface) {
+                $elementIdentifiers[$elementName] = $elementIdentifier;
+            }
+        }
+
+        if (!empty($elementIdentifiers)) {
+            $step = $step->withElementIdentifiers($elementIdentifiers);
+        }
+
+        return $step;
+    }
+}
---
+++ tests/Unit/StepFactoryTest.php
@@ -0,0 +1,350 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\BasilModelFactory\Tests\Unit;
+
+use webignition\BasilContextAwareException\ExceptionContext\ExceptionContext;
+use webignition\BasilContextAwareException\ExceptionContext\ExceptionContextInterface;
+use webignition\BasilModel\Action\ActionTypes;
+use webignition\BasilModel\Action\InputAction;
+use webignition\BasilModel\Action\InteractionAction;
+use webignition\BasilModel\Assertion\Assertion;
+use webignition\BasilModel\Assertion\AssertionComparisons;
+use webignition\BasilModel\DataSet\DataSet;
+use webignition\BasilModel\Identifier\Identifier;
+use webignition\BasilModel\Identifier\IdentifierTypes;
+use webignition\BasilModel\Step\PendingImportResolutionStep;
+use webignition\BasilModel\Step\Step;
+use webignition\BasilModel\Step\StepInterface;
+use webignition\BasilModel\Value\Value;
+use webignition\BasilModel\Value\ValueTypes;
+use webignition\BasilDataStructure\Step as StepData;
+use webignition\BasilModelFactory\MalformedPageElementReferenceException;
+use webignition\BasilModelFactory\StepFactory;
+
+class StepFactoryTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var StepFactory
+     */
+    private $stepFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->stepFactory = StepFactory::createFactory();
+    }
+
+    /**
+     * @dataProvider createFromStepDataDataProvider
+     */
+    public function testCreateFromStepData(StepData $stepData, StepInterface $expectedStep)
+    {
+        $step = $this->stepFactory->createFromStepData($stepData);
+
+        $this->assertEquals($expectedStep, $step);
+    }
+
+    public function createFromStepDataDataProvider(): array
+    {
+        return [
+            'empty step data' => [
+                'stepData' => new StepData([]),
+                'expectedStep' => new Step([], []),
+            ],
+            'empty actions and empty assertions' => [
+                'stepData' => new StepData([
+                    StepData::KEY_ACTIONS => [
+                        '',
+                        ' ',
+                    ],
+                    StepData::KEY_ASSERTIONS => [
+                        '',
+                        ' ',
+                    ],
+                ]),
+                'expectedStep' => new Step([], []),
+            ],
+            'actions only' => [
+                'stepData' => new StepData([
+                    StepData::KEY_ACTIONS => [
+                        'click ".selector"',
+                        'set ".input" to "value"',
+                    ],
+                ]),
+                'expectedStep' => new Step(
+                    [
+                        new InteractionAction(
+                            'click ".selector"',
+                            ActionTypes::CLICK,
+                            new Identifier(
+                                IdentifierTypes::CSS_SELECTOR,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    '.selector'
+                                )
+                            ),
+                            '".selector"'
+                        ),
+                        new InputAction(
+                            'set ".input" to "value"',
+                            new Identifier(
+                                IdentifierTypes::CSS_SELECTOR,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    '.input'
+                                )
+                            ),
+                            new Value(
+                                ValueTypes::STRING,
+                                'value'
+                            ),
+                            '".input" to "value"'
+                        )
+                    ],
+                    []
+                ),
+            ],
+            'assertions only' => [
+                'stepData' => new StepData([
+                    StepData::KEY_ASSERTIONS => [
+                        '".selector" is "value"',
+                        '".input" exists'
+                    ],
+                ]),
+                'expectedStep' => new Step(
+                    [
+                    ],
+                    [
+                        new Assertion(
+                            '".selector" is "value"',
+                            new Identifier(
+                                IdentifierTypes::CSS_SELECTOR,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    '.selector'
+                                )
+                            ),
+                            AssertionComparisons::IS,
+                            new Value(
+                                ValueTypes::STRING,
+                                'value'
+                            )
+                        ),
+                        new Assertion(
+                            '".input" exists',
+                            new Identifier(
+                                IdentifierTypes::CSS_SELECTOR,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    '.input'
+                                )
+                            ),
+                            AssertionComparisons::EXISTS
+                        ),
+                    ]
+                ),
+            ],
+            'page model element references' => [
+                'stepData' => new StepData([
+                    StepData::KEY_ACTIONS => [
+                        'click page_import_name.elements.element_name'
+                    ],
+                    StepData::KEY_ASSERTIONS => [
+                        'page_import_name.elements.element_name exists'
+                    ],
+                ]),
+                'expectedStep' => new Step(
+                    [
+                        new InteractionAction(
+                            'click page_import_name.elements.element_name',
+                            ActionTypes::CLICK,
+                            new Identifier(
+                                IdentifierTypes::PAGE_MODEL_ELEMENT_REFERENCE,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    'page_import_name.elements.element_name'
+                                )
+                            ),
+                            'page_import_name.elements.element_name'
+                        )
+                    ],
+                    [
+                        new Assertion(
+                            'page_import_name.elements.element_name exists',
+                            new Identifier(
+                                IdentifierTypes::PAGE_MODEL_ELEMENT_REFERENCE,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    'page_import_name.elements.element_name'
+                                )
+                            ),
+                            AssertionComparisons::EXISTS
+                        ),
+                    ]
+                ),
+            ],
+            'import name only' => [
+                'stepData' => new StepData([
+                    StepData::KEY_USE => 'import_name',
+                ]),
+                'expectedStep' => new PendingImportResolutionStep(new Step([], []), 'import_name', ''),
+            ],
+            'data provider name only' => [
+                'stepData' => new StepData([
+                    StepData::KEY_DATA => 'data_provider_import_name',
+                ]),
+                'expectedStep' => new PendingImportResolutionStep(new Step([], []), '', 'data_provider_import_name'),
+            ],
+            'import name and data provider name' => [
+                'stepData' => new StepData([
+                    StepData::KEY_USE => 'import_name',
+                    StepData::KEY_DATA => 'data_provider_import_name',
+                ]),
+                'expectedStep' => new PendingImportResolutionStep(
+                    new Step([], []),
+                    'import_name',
+                    'data_provider_import_name'
+                ),
+            ],
+            'import name and inline data' => [
+                'stepData' => new StepData([
+                    StepData::KEY_USE => 'import_name',
+                    StepData::KEY_DATA => [
+                        'data_set_1' => [
+                            'expected_title' => 'Foo',
+                        ],
+                    ]
+                ]),
+                'expectedStep' => (new PendingImportResolutionStep(
+                    new Step([], []),
+                    'import_name',
+                    ''
+                ))->withDataSets([
+                    'data_set_1' => new DataSet([
+                        'expected_title' => 'Foo',
+                    ]),
+                ]),
+            ],
+            'import name and page imported page elements' => [
+                'stepData' => new StepData([
+                    StepData::KEY_USE => 'import_name',
+                    StepData::KEY_ELEMENTS => [
+                        'heading' => 'page_import_name.elements.heading'
+                    ],
+                ]),
+                'expectedStep' => (new PendingImportResolutionStep(
+                    new Step([], []),
+                    'import_name',
+                    ''
+                ))->withElementIdentifiers([
+                    'heading' => new Identifier(
+                        IdentifierTypes::PAGE_MODEL_ELEMENT_REFERENCE,
+                        new Value(
+                            ValueTypes::STRING,
+                            'page_import_name.elements.heading'
+                        ),
+                        1,
+                        'heading'
+                    ),
+                ]),
+            ],
+            'import name, data provider name, actions and assertions' => [
+                'stepData' => new StepData([
+                    StepData::KEY_USE => 'import_name',
+                    StepData::KEY_DATA => 'data_provider_import_name',
+                    StepData::KEY_ACTIONS => [
+                        'click ".selector"',
+                    ],
+                    StepData::KEY_ASSERTIONS => [
+                        '".selector" exists',
+                    ],
+                ]),
+                'expectedStep' => new PendingImportResolutionStep(
+                    new Step(
+                        [
+                            new InteractionAction(
+                                'click ".selector"',
+                                ActionTypes::CLICK,
+                                new Identifier(
+                                    IdentifierTypes::CSS_SELECTOR,
+                                    new Value(
+                                        ValueTypes::STRING,
+                                        '.selector'
+                                    )
+                                ),
+                                '".selector"'
+                            ),
+                        ],
+                        [
+                            new Assertion(
+                                '".selector" exists',
+                                new Identifier(
+                                    IdentifierTypes::CSS_SELECTOR,
+                                    new Value(
+                                        ValueTypes::STRING,
+                                        '.selector'
+                                    )
+                                ),
+                                AssertionComparisons::EXISTS
+                            ),
+                        ]
+                    ),
+                    'import_name',
+                    'data_provider_import_name'
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider applyContextToMalformedPageElementReferenceExceptionDataProvider
+     */
+    public function testApplyContextToMalformedPageElementReferenceException(
+        StepData $stepData,
+        ExceptionContext $expectedExceptionContext
+    ) {
+        try {
+            $this->stepFactory->createFromStepData($stepData);
+
+            $this->fail('MalformedPageElementReferenceException not thrown');
+        } catch (MalformedPageElementReferenceException $malformedPageElementReferenceException) {
+            $this->assertEquals(
+                $expectedExceptionContext,
+                $malformedPageElementReferenceException->getExceptionContext()
+            );
+        }
+    }
+
+    public function applyContextToMalformedPageElementReferenceExceptionDataProvider(): array
+    {
+        return [
+            'within action string' => [
+                'stepData' => new StepData([
+                    StepData::KEY_ACTIONS => [
+                        'click page.element',
+                    ],
+                ]),
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => null,
+                    ExceptionContextInterface::KEY_STEP_NAME => null,
+                    ExceptionContextInterface::KEY_CONTENT => 'click page.element',
+                ]),
+            ],
+            'within assertion string' => [
+                'stepData' => new StepData([
+                    StepData::KEY_ASSERTIONS => [
+                        'page.element exists',
+                    ],
+                ]),
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => null,
+                    ExceptionContextInterface::KEY_STEP_NAME => null,
+                    ExceptionContextInterface::KEY_CONTENT => 'page.element exists',
+                ]),
+            ],
+        ];
+    }
+}
